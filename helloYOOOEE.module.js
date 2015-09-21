a285f2b3eea1c2ff2db2c17adba6382a5b047611
Joe Staroba <joe@staroba.com>
2015-09-21 13:20:15 -0500

    Initial commit.

---
+++ helloYOOOEE.module.js
@@ -0,0 +1,12 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('helloYOOOEE', [])
+		.controller('helloYOOOEECtrl', helloYOOOEECtrl);
+
+		function helloYOOOEECtrl(){
+			alert('hello YOOOEE');
+		}
+
+	})();
